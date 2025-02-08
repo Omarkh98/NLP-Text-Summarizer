6cac12924ce5d443052e9c28616a97a93b04ae32
Omar Khaled <34315215+Omarkh98@users.noreply.github.com>
2025-02-08 01:31:21 +0100

    Initial Project Setup and Template

---
+++ template.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level = logging.INFO, format = '[%(asctime)s]: %(message)s:')
+
+project_name = "TextSummarizer"
+
+project_structure = [
+    ".github/workflows/.gitkeep", # For CI/CD Deployment
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb" # For Jupyter Notebooks and EDA
+]
+
+for file_path in project_structure:
+    file_path = Path(file_path)
+    file_dir, file_name = os.path.split(file_path)
+    
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok = True)
+        logging.info(f"Creating directory: {file_dir} for the file {file_name}")
+
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {file_path}")
+    else:
+        logging.info(f"{file_name} already exists")
